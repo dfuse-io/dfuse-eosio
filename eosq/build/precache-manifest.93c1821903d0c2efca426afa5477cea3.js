33f72fa4674334adeb5be2dd668200997a754808
Julien Cassis <juliencassis@gmail.com>
2020-04-16 09:43:03 -0400

    must commit latest eos build.

---
+++ eosq/build/precache-manifest.93c1821903d0c2efca426afa5477cea3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb77e6897d13b782da54ef717502070b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "250edc54390379d0e6c5",
+    "url": "/static/css/2.7ec58c39.chunk.css"
+  },
+  {
+    "revision": "1bb4980613800ba2b1cf",
+    "url": "/static/css/main.3dd12e73.chunk.css"
+  },
+  {
+    "revision": "250edc54390379d0e6c5",
+    "url": "/static/js/2.d1ae72a3.chunk.js"
+  },
+  {
+    "revision": "de31ea7ae7603e8844c465e6f2df923f",
+    "url": "/static/js/2.d1ae72a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb4980613800ba2b1cf",
+    "url": "/static/js/main.bc15a8ae.chunk.js"
+  },
+  {
+    "revision": "f26a5c4fe73e5e439b49",
+    "url": "/static/js/runtime-main.224fcd07.js"
+  }
+]);
