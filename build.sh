61518dca776bf6e2027a905e28ba692360a12cac
Shaq Kheder <shaq@cryptomechanics.com>
2020-04-18 11:32:46 -0400

    update build.sh to use build directory

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-
-go generate
-CGO_ENABLED=1 GOOS=linux go build -o dfuseeos-v0.1.1-linux-amd64  ./cmd/dfuseeos
-CGO_ENABLED=1 GOOS=darwin go build -o dfuseeos-v0.1.1-darwin-amd64  ./cmd/dfuseeos
-rm ./dashboard/rice-box.go
+CWD=`dirname "$0"`
+FPATH=`( cd "$CWD" && pwd )`
+VERSION=`git describe --tags`
+NAME="dfuseeos-${VERSION}"
+BINPATH=$FPATH/build/$NAME
+printf "\nBuilding $NAME ...\n\n"
+if go build -o $BINPATH  $FPATH/cmd/dfuseeos; then 
+    printf '\e[1;32m%-6s\e[m\n' "Build Successful"
+    printf "Created $BINPATH\n"
+    printf "\nTo make 'dfuseeos' available system-wide:\n"
+    printf "sudo cp $BINPATH /usr/local/bin/dfuseeos\n\n"
+else 
+    printf '\n\e[1;31m%-6s\e[m\n' "Build Failed"
+fi
