2cf015b6d570956dff09f64095c6254fb6014710
billettc <charles@cbillette.com>
2020-04-22 13:56:42 -0400

    fix wrong re-assignation to path var

--- launcher/cli/paths.go
+++ launcher/cli/paths.go
@@ -6,7 +6,7 @@ import (
 )
 
 func CheckNodeosInstallation(path string) error {
-	if path, err := exec.LookPath(path); err != nil {
+	if _, err := exec.LookPath(path); err != nil {
 		return fmt.Errorf("WARN: could not find %s (not in PATH).  Check https://github.com/dfuse-io/dfuse-eosio/blob/develop/INSTALL.md for instructions.", path)
 	}
 
