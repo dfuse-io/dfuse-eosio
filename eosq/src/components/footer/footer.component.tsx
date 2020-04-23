7f941bd3ba3ac59c12602927fd48d4f6a9d032e1
Phil Rivard <philrivard@Phils-MacBook-Pro.local>
2020-04-22 21:47:00 -0400

    removed eoscanada and more on eosq.app

--- eosq/src/components/footer/footer.component.tsx
+++ eosq/src/components/footer/footer.component.tsx
@@ -9,7 +9,7 @@ import { fontSize, space } from "styled-system"
 import { theme, styled } from "../../theme"
 import { faTelegramPlane } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { NBSP } from "../../helpers/formatters"
+import { BULLET, NBSP } from "../../helpers/formatters"
 import { getCurrentLanguageValue } from "../settings-selectors/settings.helpers"
 
 const LogoLink: React.ComponentType<any> = styled(Link)`
@@ -38,10 +38,10 @@ const LogoSecond: React.ComponentType<any> = styled.div`
   position: relative;
 `
 
-const StyledLogoImg: React.ComponentType<any> = styled.img`
-  max-height: 16px;
+/* const StyledLogoImg: React.ComponentType<any> = styled.img`
+  max-height: 50px;
   margin-bottom: 6px;
-`
+` */
 
 const StyledText: React.ComponentType<any> = styled(Text)`
   &:hover {
@@ -77,19 +77,18 @@ const BaseFooter = () => (
               q
             </LogoSecond>
           </LogoLink>
-          <Cell pt={[3]}>
-            <Text color={theme.colors.primary}>{t("footer.subTitle")}</Text>
-          </Cell>
-          <ExternalTextLink to="https://eoscanada.com">
-            <StyledLogoImg src="/images/logo-eos-canada.svg" />
-          </ExternalTextLink>
+          {/* <Cell pt={[3]}>
+          <Text color={theme.colors.primary}>{t("footer.subTitle")}</Text>
+        </Cell>
+        <ExternalTextLink to="https://dfuse.io/">
+          <StyledLogoImg src="/images/dfuse-logo-white-01.svg" />
+        </ExternalTextLink> */}
         </Cell>
         <Cell justifySelf="right" textAlign="right" alignSelf="right" pt={[4]}>
-          <ExternalTextLink to="https://t.me/eoscanada">
+          <ExternalTextLink to="https://t.me/dfuseAPI">
             <StyledText pr={[2]} display="inline-block" color={theme.colors.bleu6}>
               {t("footer.telegram")}
             </StyledText>
-            {NBSP}
             <StyledFont color={theme.colors.bleu6} icon={faTelegramPlane} size="lg" />
           </ExternalTextLink>
         </Cell>
@@ -107,30 +106,34 @@ const BaseFooter = () => (
       textAlign="center"
     >
       <a
-        href={`https://www.dfuse.io/${getCurrentLanguageValue()}`}
-        title="dfuse API for EOS"
+        href={`https://dfuse.io/${getCurrentLanguageValue()}`}
+        title="The dfuse Blockchain Data Platform"
         target="_blank"
       >
         <img
-          src={`https://www.dfuse.io/hubfs/built-with-dfuse${
-            getCurrentLanguageValue() === "zh" ? "-CN" : ""
+          src={`/images/built-with-dfuse${
+              getCurrentLanguageValue() === "zh" ? "-CN" : ""
           }-01.png`}
-          title="dfuse API for EOS"
+          title="The dfuse Blockchain Data Platform"
           width="210"
           height="auto"
         />
+        <Text color={theme.colors.bleu6} fontSize={[1]}>
+          {t("footer.aboutDfuse")}
+        </Text>
       </a>
     </Cell>
     <Cell p={[3]} mt={[3]} borderTop={`1px solid ${theme.colors.bleu10}`} textAlign="center">
       <Text color={theme.colors.primary} fontSize={[1]}>
         {t("footer.copyright", { year: new Date().getFullYear() })}
-        &nbsp;
+      </Text>
+      <Text color={theme.colors.primary} fontSize={[1]}>
         <ExternalTextLink fontSize={[1]} to={t("footer.privacyPolicyLink")}>
           <StyledText display="inline-block" fontSize={[1]} color={theme.colors.bleu6}>
             {t("footer.privacyPolicy")}
           </StyledText>
         </ExternalTextLink>
-        .{NBSP}
+        {NBSP}{BULLET}{NBSP}
         <ExternalTextLink fontSize={[1]} to={t("footer.termsOfServicesLink")}>
           <StyledText display="inline-block" fontSize={[1]} color={theme.colors.bleu6}>
             {t("footer.termsOfServices")}
