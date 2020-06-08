a35e71b0a5ee0af710962c391dc34af768d81fd4
Stéphane Duchesneau <stephane.duchesneau@eoscanada.com>
2020-06-08 16:55:28 -0400

    dgraphql trxdb first pass improve trxEvents lookup

--- trxdb/kv/read_transactions.go
+++ trxdb/kv/read_transactions.go
@@ -1,217 +1,228 @@
 package kv
 
 import (
+	"bytes"
 	"context"
 
 	pbcodec "github.com/dfuse-io/dfuse-eosio/pb/dfuse/eosio/codec/v1"
 	pbtrxdb "github.com/dfuse-io/dfuse-eosio/pb/dfuse/eosio/trxdb/v1"
-	"github.com/dfuse-io/kvdb"
 	"github.com/dfuse-io/kvdb/store"
 	"github.com/eoscanada/eos-go"
 )
 
+type TrxEventType int
+
+const (
+	TrxAdditionEvent TrxEventType = iota + 1
+	TrxExecutionEvent
+	ImplicitTrxEvent
+	DtrxEvent
+)
+
 func (db *DB) GetTransactionTraces(ctx context.Context, idPrefix string) (out []*pbcodec.TransactionEvent, err error) {
-	out, err = db.getTransactionExecutionEvents(ctx, idPrefix)
+	out, err = db.getTransactionEvents(ctx, idPrefix, TrxExecutionEvent)
+	if err != nil {
+		return nil, err
+	}
+	err = db.fillIrreversibilityData(ctx, out)
+	return
+}
+
+func (db *DB) GetTransactionEvents(ctx context.Context, idPrefix string) (out []*pbcodec.TransactionEvent, err error) {
+	out, err = db.getTransactionEvents(ctx, idPrefix)
+	if err != nil {
+		return nil, err
+	}
+	err = db.fillIrreversibilityData(ctx, out)
 	return
 }
 
 func (db *DB) GetTransactionEventsBatch(ctx context.Context, idPrefixes []string) (out [][]*pbcodec.TransactionEvent, err error) {
 	// OPTIMIZE: Parallelize access, or do requests to get things in parallel
 	for _, idPrefix := range idPrefixes {
-		trxResult, err := db.GetTransactionEvents(ctx, idPrefix)
+		trxResult, err := db.getTransactionEvents(ctx, idPrefix)
 		if err != nil {
 			return nil, err
 		}
 		out = append(out, trxResult)
 	}
+	err = db.fillIrreversibilityDataArray(ctx, out)
 	return
 }
 
 func (db *DB) GetTransactionTracesBatch(ctx context.Context, idPrefixes []string) (out [][]*pbcodec.TransactionEvent, err error) {
 	// OPTIMIZE: Parallelize access, or do requests to get things in parallel
 	for _, idPrefix := range idPrefixes {
-		trxResult, err := db.GetTransactionTraces(ctx, idPrefix)
+		trxResult, err := db.getTransactionEvents(ctx, idPrefix, TrxExecutionEvent)
 		if err != nil {
 			return nil, err
 		}
 		out = append(out, trxResult)
 	}
+	err = db.fillIrreversibilityDataArray(ctx, out)
 	return
 }
 
-func (db *DB) GetTransactionEvents(ctx context.Context, idPrefix string) (out []*pbcodec.TransactionEvent, err error) {
-	evs, err := db.getTransactionAdditionEvents(ctx, idPrefix)
-	if err != nil {
-		return nil, err
+func (db *DB) fillIrreversibilityDataArray(ctx context.Context, eventsArray [][]*pbcodec.TransactionEvent) error {
+	var flatEvs []*pbcodec.TransactionEvent
+	for _, evs := range eventsArray {
+		flatEvs = append(flatEvs, evs...)
 	}
-	out = append(out, evs...)
-
-	evs, err = db.getTransactionExecutionEvents(ctx, idPrefix)
-	if err != nil {
-		return nil, err
-	}
-	out = append(out, evs...)
-
-	evs, err = db.getTransactionDtrxEvents(ctx, idPrefix)
-	if err != nil {
-		return nil, err
-	}
-	out = append(out, evs...)
-
-	evs, err = db.getTransactionImplicitEvents(ctx, idPrefix)
-	if err != nil {
-		return nil, err
-	}
-	out = append(out, evs...)
-
-	return
+	return db.fillIrreversibilityData(ctx, flatEvs)
 }
 
-func (db *DB) getTransactionAdditionEvents(ctx context.Context, idPrefix string) (out []*pbcodec.TransactionEvent, err error) {
-	it := db.store.Prefix(ctx, Keys.PackTrxsPrefix(idPrefix), store.Unlimited)
+func (db *DB) fillIrreversibilityData(ctx context.Context, events []*pbcodec.TransactionEvent) error {
+	blockIDs := make(map[string]bool)
+	for _, ev := range events {
+		blockIDs[ev.BlockId] = false
+	}
+	var prefixes [][]byte
+	for id := range blockIDs {
+		prefixes = append(prefixes, Keys.PackIrrBlocksKey(id))
+	}
+
+	it := db.store.BatchGet(ctx, prefixes)
 	for it.Next() {
-		row := &pbtrxdb.TrxRow{}
-		db.dec.MustInto(it.Item().Value, row)
+		blockIDs[Keys.UnpackIrrBlocksKey(it.Item().Key)] = true
+	}
+	if it.Err() != nil && it.Err() != store.ErrNotFound {
+		return it.Err()
+	}
 
-		trxID, blockID := Keys.UnpackTrxsKey(it.Item().Key)
+	for _, ev := range events {
+		ev.Irreversible = blockIDs[ev.BlockId]
+	}
 
-		ev := &pbcodec.TransactionEvent{
-			Id:       trxID,
-			BlockId:  blockID,
-			BlockNum: eos.BlockNum(blockID),
+	return nil
+}
+
+func (db *DB) getTransactionEvents(ctx context.Context, idPrefix string, eventTypes ...TrxEventType) (out []*pbcodec.TransactionEvent, err error) {
+	var keys [][]byte
+	if len(eventTypes) == 0 { //default behavior is get all events
+		eventTypes = []TrxEventType{TrxAdditionEvent, TrxExecutionEvent, ImplicitTrxEvent, DtrxEvent}
+	}
+	for _, t := range eventTypes {
+		switch t {
+		case TrxAdditionEvent:
+			keys = append(keys, Keys.PackTrxsPrefix(idPrefix))
+		case TrxExecutionEvent:
+			keys = append(keys, Keys.PackTrxTracesPrefix(idPrefix))
+		case ImplicitTrxEvent:
+			keys = append(keys, Keys.PackImplicitTrxsPrefix(idPrefix))
+		case DtrxEvent:
+			keys = append(keys, Keys.PackDtrxsPrefix(idPrefix))
+		default:
+			panic("invalid trx event")
 		}
-		_, err := db.store.Get(ctx, Keys.PackIrrBlocksKey(blockID))
-		ev.Irreversible = err != store.ErrNotFound
+	}
 
-		if row.Receipt != nil {
-			ev.Event = &pbcodec.TransactionEvent_Addition{
-				Addition: &pbcodec.TransactionEvent_Added{
-					Receipt:     row.Receipt,
-					Transaction: row.SignedTrx,
-					PublicKeys:  row.PublicKeys,
-				},
+	it := db.store.BatchPrefix(ctx, keys, store.Unlimited)
+	for it.Next() {
+		switch {
+		// Implicit Transaction Addition
+		case bytes.HasPrefix(it.Item().Key, Keys.StartOfImplicitTrxsTable()):
+			row := &pbtrxdb.ImplicitTrxRow{}
+			db.dec.MustInto(it.Item().Value, row)
+
+			trxID, blockID := Keys.UnpackImplicitTrxsKey(it.Item().Key)
+			ev := &pbcodec.TransactionEvent{
+				Id:       trxID,
+				BlockId:  blockID,
+				BlockNum: eos.BlockNum(blockID),
 			}
-		} else {
 			ev.Event = &pbcodec.TransactionEvent_InternalAddition{
 				InternalAddition: &pbcodec.TransactionEvent_AddedInternally{
 					Transaction: row.SignedTrx,
 				},
 			}
-		}
+			out = append(out, ev)
 
-		out = append(out, ev)
-	}
-	if err := it.Err(); err != nil {
-		return nil, err
+		// Transaction Addition
+		case bytes.HasPrefix(it.Item().Key, Keys.StartOfTrxsTable()):
+			row := &pbtrxdb.TrxRow{}
+			db.dec.MustInto(it.Item().Value, row)
+
+			trxID, blockID := Keys.UnpackTrxsKey(it.Item().Key)
+
+			ev := &pbcodec.TransactionEvent{
+				Id:       trxID,
+				BlockId:  blockID,
+				BlockNum: eos.BlockNum(blockID),
+			}
+
+			if row.Receipt != nil {
+				ev.Event = &pbcodec.TransactionEvent_Addition{
+					Addition: &pbcodec.TransactionEvent_Added{
+						Receipt:     row.Receipt,
+						Transaction: row.SignedTrx,
+						PublicKeys:  row.PublicKeys,
+					},
+				}
+			} else {
+				ev.Event = &pbcodec.TransactionEvent_InternalAddition{
+					InternalAddition: &pbcodec.TransactionEvent_AddedInternally{
+						Transaction: row.SignedTrx,
+					},
+				}
+			}
+
+			out = append(out, ev)
+
+		// Transaction Execution
+		case bytes.HasPrefix(it.Item().Key, Keys.StartOfTrxTracesTable()):
+			row := &pbtrxdb.TrxTraceRow{}
+			db.dec.MustInto(it.Item().Value, row)
+
+			trxID, blockID := Keys.UnpackTrxTracesKey(it.Item().Key)
+
+			ev := &pbcodec.TransactionEvent{
+				Id:       trxID,
+				BlockId:  blockID,
+				BlockNum: eos.BlockNum(blockID),
+			}
+
+			ev.Event = &pbcodec.TransactionEvent_Execution{
+				Execution: &pbcodec.TransactionEvent_Executed{
+					Trace:       row.TrxTrace,
+					BlockHeader: row.BlockHeader,
+				},
+			}
+
+			out = append(out, ev)
+
+		// Deferred Trx
+		case bytes.HasPrefix(it.Item().Key, Keys.StartOfDtrxsTable()):
+			row := &pbtrxdb.DtrxRow{}
+			db.dec.MustInto(it.Item().Value, row)
+
+			trxID, blockID := Keys.UnpackDtrxsKey(it.Item().Key)
+
+			ev := &pbcodec.TransactionEvent{
+				Id:       trxID,
+				BlockId:  blockID,
+				BlockNum: eos.BlockNum(blockID),
+			}
+
+			if row.CreatedBy != nil {
+				ev.Event = &pbcodec.TransactionEvent_DtrxScheduling{
+					DtrxScheduling: &pbcodec.TransactionEvent_DtrxScheduled{
+						CreatedBy:   row.CreatedBy,
+						Transaction: row.SignedTrx,
+					},
+				}
+			} else {
+				ev.Event = &pbcodec.TransactionEvent_DtrxCancellation{
+					DtrxCancellation: &pbcodec.TransactionEvent_DtrxCanceled{
+						CanceledBy: row.CanceledBy,
+					},
+				}
+			}
+
+			out = append(out, ev)
+		}
 	}
 
 	// TODO: does bigtable return `ErrNotFound` when none are found here?
-
-	return
-}
-
-func (db *DB) getTransactionImplicitEvents(ctx context.Context, idPrefix string) (out []*pbcodec.TransactionEvent, err error) {
-	it := db.store.Prefix(ctx, Keys.PackImplicitTrxsPrefix(idPrefix), store.Unlimited)
-	for it.Next() {
-		row := &pbtrxdb.ImplicitTrxRow{}
-		db.dec.MustInto(it.Item().Value, row)
-
-		trxID, blockID := Keys.UnpackImplicitTrxsKey(it.Item().Key)
-
-		ev := &pbcodec.TransactionEvent{
-			Id:       trxID,
-			BlockId:  blockID,
-			BlockNum: eos.BlockNum(blockID),
-		}
-		_, err := db.store.Get(ctx, Keys.PackIrrBlocksKey(blockID))
-		ev.Irreversible = err != store.ErrNotFound
-
-		ev.Event = &pbcodec.TransactionEvent_InternalAddition{
-			InternalAddition: &pbcodec.TransactionEvent_AddedInternally{
-				Transaction: row.SignedTrx,
-			},
-		}
-
-		out = append(out, ev)
-	}
-	if err := it.Err(); err != nil {
-		return nil, err
-	}
-
-	// TODO: does bigtable return `ErrNotFound` when none are found here?
-
-	return
-}
-
-func (db *DB) getTransactionExecutionEvents(ctx context.Context, idPrefix string) (out []*pbcodec.TransactionEvent, err error) {
-	it := db.store.Prefix(ctx, Keys.PackTrxTracesPrefix(idPrefix), store.Unlimited)
-	for it.Next() {
-		row := &pbtrxdb.TrxTraceRow{}
-		db.dec.MustInto(it.Item().Value, row)
-
-		trxID, blockID := Keys.UnpackTrxTracesKey(it.Item().Key)
-
-		ev := &pbcodec.TransactionEvent{
-			Id:       trxID,
-			BlockId:  blockID,
-			BlockNum: eos.BlockNum(blockID),
-		}
-		_, err := db.store.Get(ctx, Keys.PackIrrBlocksKey(blockID))
-		ev.Irreversible = err != store.ErrNotFound
-
-		ev.Event = &pbcodec.TransactionEvent_Execution{
-			Execution: &pbcodec.TransactionEvent_Executed{
-				Trace:       row.TrxTrace,
-				BlockHeader: row.BlockHeader,
-			},
-		}
-
-		out = append(out, ev)
-	}
-	if err := it.Err(); err != nil {
-		return nil, err
-	}
-
-	if len(out) == 0 {
-		return nil, kvdb.ErrNotFound
-	}
-
-	return
-}
-
-func (db *DB) getTransactionDtrxEvents(ctx context.Context, idPrefix string) (out []*pbcodec.TransactionEvent, err error) {
-	it := db.store.Prefix(ctx, Keys.PackDtrxsPrefix(idPrefix), store.Unlimited)
-	for it.Next() {
-		row := &pbtrxdb.DtrxRow{}
-		db.dec.MustInto(it.Item().Value, row)
-
-		trxID, blockID := Keys.UnpackDtrxsKey(it.Item().Key)
-
-		ev := &pbcodec.TransactionEvent{
-			Id:       trxID,
-			BlockId:  blockID,
-			BlockNum: eos.BlockNum(blockID),
-		}
-		_, err := db.store.Get(ctx, Keys.PackIrrBlocksKey(blockID))
-		ev.Irreversible = err != store.ErrNotFound
-
-		if row.CreatedBy != nil {
-			ev.Event = &pbcodec.TransactionEvent_DtrxScheduling{
-				DtrxScheduling: &pbcodec.TransactionEvent_DtrxScheduled{
-					CreatedBy:   row.CreatedBy,
-					Transaction: row.SignedTrx,
-				},
-			}
-		} else {
-			ev.Event = &pbcodec.TransactionEvent_DtrxCancellation{
-				DtrxCancellation: &pbcodec.TransactionEvent_DtrxCanceled{
-					CanceledBy: row.CanceledBy,
-				},
-			}
-		}
-
-		out = append(out, ev)
-	}
 	if err := it.Err(); err != nil {
 		return nil, err
 	}
