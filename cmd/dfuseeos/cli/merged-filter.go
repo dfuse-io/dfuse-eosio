908ed7e6419aa9f31239929a51c5fec13071f72e
Stéphane Duchesneau <stephane.duchesneau@eoscanada.com>
2020-07-27 14:41:05 -0400

    add batch mode to mergedfilter

--- cmd/dfuseeos/cli/merged-filter.go
+++ cmd/dfuseeos/cli/merged-filter.go
@@ -18,6 +18,10 @@ func init() {
 			cmd.Flags().Bool("merged-filter-truncation-enabled", true, "[NOT IMPLEMENTED] Will delete filtered merged blocks files after the truncation window period.")
 			cmd.Flags().Uint64("merged-filter-truncation-window", 0, "Number of blocks to keep history of filtered merged blocks. Used as start position when no filtered files exist.")
 			cmd.Flags().String("merged-filter-destination-blocks-store-url", FilteredBlocksStoreURL, "Object Store where to write filtered blocks store.  Sources from --common-blocks-store-url.")
+
+			cmd.Flags().Bool("merged-filter-batch-mode", false, "Use this to explicitly set start/stop block numbers for processing and ignore current chain head")
+			cmd.Flags().Uint64("merged-filter-batch-start-block", 0, "When running in batch mode, block number (rounded) where to start processing")
+			cmd.Flags().Uint64("merged-filter-batch-stop-block", 0, "When running in batch mode, block number (rounded) where to stop processing")
 			return nil
 		},
 		InitFunc: func(runtime *launcher.Runtime) (err error) {
@@ -36,6 +40,9 @@ func init() {
 				SourceBlocksStoreURL: mustReplaceDataDir(dfuseDataDir, viper.GetString("common-blocks-store-url")),
 				TruncationEnabled:    viper.GetBool("merged-filter-truncation-enabled"),
 				TruncationWindow:     viper.GetUint64("merged-filter-truncation-window"),
+				BatchMode:            viper.GetBool("merged-filter-batch-mode"),
+				BatchStartBlock:      viper.GetUint64("merged-filter-batch-start-block"),
+				BatchStopBlock:       viper.GetUint64("merged-filter-batch-stop-block"),
 				IncludeFilterExpr:    viper.GetString("common-include-filter-expr"),
 				ExcludeFilterExpr:    viper.GetString("common-exclude-filter-expr"),
 				BlockstreamAddr:      viper.GetString("common-blockstream-addr"),
