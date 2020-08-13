d002b175eed2cf9fa7e058131bfccde812acf409
Stéphane Duchesneau <stephane.duchesneau@eoscanada.com>
2020-08-13 15:39:52 -0400

    Bump merger, fix #90, fix #86, fix #89, fix #112, fix #96
    * adds --merger-one-block-deletion-threads
    * adds --merger-max-one-block-operations-batch-size

--- cmd/dfuseeos/cli/merger.go
+++ cmd/dfuseeos/cli/merger.go
@@ -27,7 +27,10 @@ func init() {
 			cmd.Flags().Duration("merger-writers-leeway", 10*time.Second, "how long we wait after seeing the upper boundary, to ensure that we get as many blocks as possible in a bundle")
 			cmd.Flags().String("merger-seen-blocks-file", "{dfuse-data-dir}/merger/merger.seen.gob", "file to save to / load from the map of 'seen blocks'")
 			cmd.Flags().Uint64("merger-max-fixable-fork", 10000, "after that number of blocks, a block belonging to another fork will be discarded (DELETED depending on flagDeleteBlocksBefore) instead of being inserted in last bundle")
-			cmd.Flags().Bool("merger-delete-blocks-before", true, "Enable deletion of oneblock files when prior to the currently processed bundle (to avoid long file listings)")
+			cmd.Flags().Bool("merger-delete-blocks-before", true, "Enable deletion of oneblock files when prior to the currently processed bundle and in 'seenBlocks' list (you should really keep this to True)")
+			cmd.Flags().Int("merger-one-block-deletion-threads", 10, "number of parallel threads used to delete one-block-files (more means more stress on your storage backend)")
+			cmd.Flags().Int("merger-max-one-block-operations-batch-size", 2000, "number of good files to look up from storage before we merge them")
+
 			return nil
 		},
 		// FIXME: Lots of config value construction is duplicated across InitFunc and FactoryFunc, how to streamline that
@@ -53,19 +56,21 @@ func init() {
 		FactoryFunc: func(runtime *launcher.Runtime) (launcher.App, error) {
 			dfuseDataDir := runtime.AbsDataDir
 			return mergerApp.New(&mergerApp.Config{
-				StorageMergedBlocksFilesPath: mustReplaceDataDir(dfuseDataDir, viper.GetString("common-blocks-store-url")),
-				StorageOneBlockFilesPath:     mustReplaceDataDir(dfuseDataDir, viper.GetString("common-oneblock-store-url")),
-				TimeBetweenStoreLookups:      viper.GetDuration("merger-time-between-store-lookups"),
-				GRPCListenAddr:               viper.GetString("merger-grpc-listen-addr"),
-				Live:                         viper.GetBool("merger-process-live-blocks"),
-				StartBlockNum:                viper.GetUint64("merger-start-block-num"),
-				StopBlockNum:                 viper.GetUint64("merger-stop-block-num"),
-				ProgressFilename:             viper.GetString("merger-progress-filename"),
-				MinimalBlockNum:              viper.GetUint64("merger-minimal-block-num"),
-				WritersLeewayDuration:        viper.GetDuration("merger-writers-leeway"),
-				SeenBlocksFile:               mustReplaceDataDir(dfuseDataDir, viper.GetString("merger-seen-blocks-file")),
-				MaxFixableFork:               viper.GetUint64("merger-max-fixable-fork"),
-				DeleteBlocksBefore:           viper.GetBool("merger-delete-blocks-before"),
+				StorageMergedBlocksFilesPath:   mustReplaceDataDir(dfuseDataDir, viper.GetString("common-blocks-store-url")),
+				StorageOneBlockFilesPath:       mustReplaceDataDir(dfuseDataDir, viper.GetString("common-oneblock-store-url")),
+				TimeBetweenStoreLookups:        viper.GetDuration("merger-time-between-store-lookups"),
+				GRPCListenAddr:                 viper.GetString("merger-grpc-listen-addr"),
+				Live:                           viper.GetBool("merger-process-live-blocks"),
+				StartBlockNum:                  viper.GetUint64("merger-start-block-num"),
+				StopBlockNum:                   viper.GetUint64("merger-stop-block-num"),
+				ProgressFilename:               viper.GetString("merger-progress-filename"),
+				MinimalBlockNum:                viper.GetUint64("merger-minimal-block-num"),
+				WritersLeewayDuration:          viper.GetDuration("merger-writers-leeway"),
+				SeenBlocksFile:                 mustReplaceDataDir(dfuseDataDir, viper.GetString("merger-seen-blocks-file")),
+				MaxFixableFork:                 viper.GetUint64("merger-max-fixable-fork"),
+				DeleteBlocksBefore:             viper.GetBool("merger-delete-blocks-before"),
+				MaxOneBlockOperationsBatchSize: viper.GetInt("merger-max-one-block-operations-batch-size"),
+				OneBlockDeletionThreads:        viper.GetInt("merger-one-block-deletion-threads"),
 			}), nil
 		},
 	})
--- go.mod
+++ go.mod
@@ -36,7 +36,7 @@ require (
 	github.com/dfuse-io/jsonpb v0.0.0-20200629203253-eb615da38083
 	github.com/dfuse-io/kvdb v0.0.2-0.20200807025817-f1b87abdaad6
 	github.com/dfuse-io/logging v0.0.0-20200611143916-aade15324493
-	github.com/dfuse-io/merger v0.0.3-0.20200724152216-947b231c83f9
+	github.com/dfuse-io/merger v0.0.3-0.20200813193037-208d86e877f9
 	github.com/dfuse-io/node-manager v0.0.2-0.20200813012016-72fd864261d6
 	github.com/dfuse-io/opaque v0.0.0-20200407012705-75c4ca372d71
 	github.com/dfuse-io/pbgo v0.0.6-0.20200729035815-5da1dbc5e2ac
